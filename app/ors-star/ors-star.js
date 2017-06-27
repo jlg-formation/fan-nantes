6259375dcd3cb6817851fa925d67aa6508b3bcd8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-27 16:24:52 +0200

    x

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -9,7 +9,14 @@
 			controller: function OrsStarCtrl($element){
 				'ngInject';
 				console.log('OrsStarCtrl', arguments);
-				let html = 'toto';
+				let note = 3;
+				let html = '';
+				for (let i = 0; i < note; i++) {
+					html += '<img src="./ors-star/img/yellow_star.png">';					
+				}
+				for (let i = note; i < 5; i++) {
+					html += '<img src="./ors-star/img/white_star.png">';					
+				}
 				$element.html(html);
 			}
 		};
