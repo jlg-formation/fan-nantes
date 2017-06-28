1280a10338a590793d72e6f15f438c3d3dda7ecb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-28 10:17:12 +0200

    add: import html

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ import './ors-star/ors-star.js';
 
 var app = angular.module('main', ['ors-star']);
 
-const orsHeaderUrl = require('./tmpl/ors-header.html');
+import orsHeaderUrl from './tmpl/ors-header.html';
 const orsBodyUrl = require('./tmpl/ors-body.html');
 const orsFooterUrl = require('./tmpl/ors-footer.html');
 
--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -1,42 +1,39 @@
-(function () {
-	'use strict';
+const app = angular.module('ors-star', []);
 
-	var app = angular.module('ors-star', []);
-
-	app.directive('orsStar', function () {
-		return {
-			restrict: 'E',
-			scope: {
-				note: '=?'
-			},
-			controller: function OrsStarCtrl($scope, $element, $compile) {
-				'ngInject';
-				console.log('OrsStarCtrl', arguments);
-				$scope.update = function update(newNote) {
-					console.log('newNote', newNote);
-					$scope.note = newNote;
-				};
-				$scope.$watch('note', function () {
-					console.log('test à chaud');
-					let note = $scope.note || 3;
-					note = (note > 5) ? 5 : note;
-					note = (note < 0) ? 0 : note;
-					let html = '';
-					for (let i = 0; i < note; i++) {
-						html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png">';
-					}
-					for (let i = note; i < 5; i++) {
-						html += `
+app.directive('orsStar', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			note: '=?'
+		},
+		controller: function OrsStarCtrl($scope, $element, $compile) {
+			'ngInject';
+			console.log('OrsStarCtrl', arguments);
+			$scope.update = function update(newNote) {
+				console.log('newNote', newNote);
+				$scope.note = newNote;
+			};
+			$scope.$watch('note', function () {
+				console.log('test à chaud');
+				let note = $scope.note || 3;
+				note = (note > 5) ? 5 : note;
+				note = (note < 0) ? 0 : note;
+				let html = '';
+				for (let i = 0; i < note; i++) {
+					html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png">';
+				}
+				for (let i = note; i < 5; i++) {
+					html += `
 <img  
 ng-click="update(${i + 1})" 
 src="./ors-star/img/white_star.png">`;
-					}
-					$element.html(html);
-					$compile($element.contents())($scope);
-				});
+				}
+				$element.html(html);
+				$compile($element.contents())($scope);
+			});
 
-			}
-		};
-	});
+		}
+	};
+});
 
-})();
+
